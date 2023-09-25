9fec1b35ac83b0fdc8e53acc8e49494f885628ce
Khandakar227 <xenonknight54@gmail.com>
2023-09-25 20:34:18 +0600

    Slider added

---
+++ client/src/components/Home/IntroCarousel.tsx
@@ -0,0 +1,53 @@
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import { PropsWithChildren } from "react";
+
+export default function IntroCarousel({children}:PropsWithChildren) {
+    const [ref] = useKeenSlider<HTMLDivElement>(
+    {
+    loop: true,
+    },
+    [
+        (slider) => {
+          let timeout: ReturnType<typeof setTimeout>
+          let mouseOver = false
+          function clearNextTimeout() {
+            clearTimeout(timeout)
+          }
+          function nextTimeout() {
+            clearTimeout(timeout)
+            if (mouseOver) return
+            timeout = setTimeout(() => {
+              slider.next()
+            }, 3000)
+          }
+          slider.on("created", () => {
+            slider.container.addEventListener("mouseover", () => {
+              mouseOver = true
+              clearNextTimeout()
+            })
+            slider.container.addEventListener("mouseout", () => {
+              mouseOver = false
+              nextTimeout()
+            })
+            nextTimeout()
+          })
+          slider.on("dragStarted", clearNextTimeout)
+          slider.on("animationEnded", nextTimeout)
+          slider.on("updated", nextTimeout)
+        },
+      ]
+    )
+
+    return (
+        <div ref={ref} className="keen-slider">
+            {children}
+        </div>
+  )
+}
+
+export const IntroSlide = ({children}:PropsWithChildren) => {
+    return (
+        <div className="keen-slider__slide">{children}</div>
+    )
+}
--- client/src/components/Home/IntroSection.tsx
+++ client/src/components/Home/IntroSection.tsx
@@ -1,13 +1,39 @@
+import { CSSProperties } from "react"
+import IntroCarousel, { IntroSlide } from "./IntroCarousel"
+
 export default function IntroSection() {
+  function styleBg(imgUrl:string):CSSProperties {
+    return ({
+      backgroundImage: `url(${imgUrl})`, backgroundRepeat: "no-repeat",
+      backgroundPosition: "left",
+      backgroundSize: "cover"
+    })
+  }
+
   return (
     <div className="min-h-main w-full grid justify-center items-center md:grid-cols-2">
-        <div className="pl-4 py-20">
+        <div className="lg:pl-24 pl-8 py-20">
             <p className="text-blue-300">Welcome to IUT Computer Society</p>
             <h1 className="md:text-3xl text-xl pt-2 font-kanit">Discover our journey and commitment to the computer engineering community.</h1>
             <p className="md:text-xl py-5">Explore our mission, history, and the impact we've made over the years. Join us in shaping the future of technology together.</p>
         </div>
-        <div className="intro-bg md:block hidden " />
-        <div className="intro-bg-mobile md:hidden block" />
+        <div className="clip-path max-h-[calc(100vh - 66px)] h-full md:flex hidden overflow-hidden">
+          <IntroCarousel >
+              <IntroSlide>
+                <div style={styleBg("development.jpg")} className="intro-bg w-full" />
+              </IntroSlide>
+              <IntroSlide>
+                <div style={styleBg("inter-iut-pc.jpg")} className="intro-bg w-full" />
+              </IntroSlide>
+              <IntroSlide>
+                <div style={styleBg("ctf_champion.jpg")} className="intro-bg w-full" />
+              </IntroSlide>
+              <IntroSlide>
+                <div style={styleBg("hackathon_IUT_Pixel.jpg")} className="intro-bg w-full" />
+              </IntroSlide>
+          </IntroCarousel>
+        </div>
+        <div style={styleBg("development.jpg")} className="intro-bg-mobile md:hidden block" />
 
     </div>
   )
